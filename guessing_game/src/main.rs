c4447080fd5e909e10b5f65827d3a9a1cc1c3494
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-04-30 14:25:59 +0900

    [update]num compare function

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -13,10 +14,20 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
+    io::stdin().read_line(&mut guess)
         // 例外処理
         .expect("Failed to read line");
+    
+    // 整数型を指定
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    // 数字の大小の比較
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
