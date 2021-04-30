9e56490da62515978ba8cabc1f54c97f94c48090
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2021-04-30 14:33:15 +0900

    [add]guessing_game finish

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,24 +10,36 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    // whileloop機能
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        // 例外処理
-        .expect("Failed to read line");
-    
-    // 整数型を指定
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        io::stdin().read_line(&mut guess)
+            // 例外処理
+            .expect("Failed to read line");
+        
+        // 整数型を指定、入力が違ったらもう一度入力を促す 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        // 整数型を指定
+        // let guess: u32 = guess.trim().parse()
+        //     .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    // 数字の大小の比較
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // 数字の大小の比較
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
